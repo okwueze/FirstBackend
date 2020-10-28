859c5d8833fda6a636b66c30937bb74c9bd74189
okwueze <69356352+okwueze@users.noreply.github.com>
2020-10-28 20:39:31 +0100

    k

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -5,6 +5,6 @@ const admin = require('../controllers/admin.controllers');
 router.post('/register', admin.register);
 router.post('/login', admin.login)
 
-module.exports = router;
+module.exports = router;  
 
 // once you import controllers you need to export the router and import it in server.js
